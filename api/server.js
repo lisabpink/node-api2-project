bd284fca9b4541004980163ba534efe1b5bc590b
LISA_COOK <lisabpink@gmail.com>
2020-02-11 15:28:35 -0500

    LDC GET and POST working, format files

--- api/server.js
+++ api/server.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 
-const postsRouter = require('../data/db-router');
+const postsRouter = require("../data/db-router");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send(`Hello from Server.js`);
+server.get("/", (req, res) => {
+  res.send(`Hello from Server.js`);
 });
 
-server.use('/api/posts', postsRouter);
+server.use("/api/posts", postsRouter);
 
-
-module.exports = server; 
+module.exports = server;
--- data/db-router.js
+++ data/db-router.js
@@ -1,42 +1,58 @@
-const express = require('express');
+const express = require("express");
 
-const Posts = require('./db');
+const Posts = require("./db");
 
 const router = express.Router();
 
-router.use(express.json());//might be able to remove
+router.use(express.json()); 
 
-router.post('/', (req, res) => {
-    Posts.find(req.query)
-    .then()
-    .catch(error => {
-        console.log(error);
-        res.status(500).json({ error: "There was an error while saving the post to the database" })
+//!GET requests
+router.get("/", (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
     })
-})
+    .catch(error => {
+      console.log("error on GET/api/posts", error);
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
 
-router.post('/', (req, res) => {
+router.get("/", (req, res) => {});
 
-})
+router.get("/", (req, res) => {});
 
-router.get('/', (req, res) => {
+//!POST requests
+router.post("/", (req, res) => {
+  const userPosts = req.body;
+  Posts.insert(userPosts)
+    .then(posts => {
+      userPosts.title && userPosts.contents === ""
+        ? res
+            .status(400)
+            .json({
+              errorMessage: "Please provide title and contents for the post."
+            })
+        : res.status(201).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the post to the database"
+        });
+    });
+});
 
-})
+router.post("/", (req, res) => {});
 
-router.get('/', (req, res) => {
+//!DELETE request
+router.delete("/", (req, res) => {});
 
-})
+//!PUT request
+router.put("/", (req, res) => {});
 
-router.get('/', (req, res) => {
-
-})
-
-router.delete('/', (req, res) => {
-
-})
-
-router.put('/', (req, res) => {
-
-})
-
-module.exports = router; 
+module.exports = router;
